4c5ca108acb7e5eae1d674886c3a90a66972052a
Zak Ainsworth <zakainsworth@gmail.com>
2020-12-06 03:36:38 +0000

    WAY less complicated solution to 2020/05...

--- 2020/05/code.rb
+++ 2020/05/code.rb
@@ -2,29 +2,13 @@
 
 SEAT_KEYS = File.readlines(ARGV[0]).map(&:strip)
 
-class Seat
-  MAP = { 'F' => 0, 'B' => 1, 'L' => 0, 'R' => 1 }.freeze
-  ROWS = (0..127).to_a.freeze
-  COLS = (0..7).to_a.freeze
-
-  attr_reader :id
-
-  def initialize(key)
-    @row_key, @col_key = key.split('').each_slice(7).to_a
-    @row = find_seat(ROWS, @row_key)
-    @col = find_seat(COLS, @col_key)
-    @id = @row * 8 + @col
-  end
-
-  private
-
-  def find_seat(seats, key)
-    key.each { |l| seats = seats.each_slice(seats.size / 2).to_a[MAP[l]] }
-    seats[0]
-  end
+def key_to_int(key)
+  key.gsub(/[FBLR]/, 'F' => '0', 'B' => '1', 'L' => '0', 'R' => '1').to_i(2)
 end
 
-ids = SEAT_KEYS.map { |seat_key| Seat.new(seat_key).id }
+ids = SEAT_KEYS.map do |key|
+  key_to_int(key[0,7]) * 8 + key_to_int(key[7,3])
+end
 
 puts "Part One: #{ids.max}"
 puts "Part Two: #{([*ids.min..ids.max] - ids)[0]}"
